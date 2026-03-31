273fe6b8eae6de831af768b9806257e08a860b7e
Feiran.XU@cn.bosch.com <XEF1CNG@bosch.com>
2026-03-31 10:49:20 +0800

    fix: remove redundant java.util.List import in AnalysisReportService

--- src/main/java/com/bosch/rbcc/aftermarketpartsmanagementsystem/service/AnalysisReportService.java
+++ src/main/java/com/bosch/rbcc/aftermarketpartsmanagementsystem/service/AnalysisReportService.java
@@ -17,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
