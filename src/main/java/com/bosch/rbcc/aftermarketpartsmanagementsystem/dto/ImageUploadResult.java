0766f7b6dfb29a2436339eba33abe11e8a6023e5
Feiran.XU@cn.bosch.com <XEF1CNG@bosch.com>
2026-05-11 14:23:01 +0800

    feat: add part image upload/delete endpoints with FileStorageService

---
+++ src/main/java/com/bosch/rbcc/aftermarketpartsmanagementsystem/dto/ImageUploadResult.java
@@ -0,0 +1,15 @@
+package com.bosch.rbcc.aftermarketpartsmanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageUploadResult {
+    private String relativePath;
+    private String url;
+}
